647009596e5e774b9152fc400d1059437f04f55e
a6hi <saraffabhishek@outlook.com>
2023-05-15 18:02:20 -0400

    sorted app into components

--- react-memory-card/src/App.js
+++ react-memory-card/src/App.js
@@ -1,5 +1,38 @@
-function App() {
-  return <div>App</div>;
-}
+import React, { useState, useEffect } from "react";
+
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [currentLevel, setLevel] = useState(0);
+
+  const incrementCurrentScore = () => {
+    setCurrentScore(currentScore + 1);
+  };
+
+  const incrementLevel = () => {
+    setLevel(currentLevel + 1);
+  };
+
+  const updateBestScore = () => {
+    setBestScore(currentScore);
+  };
+
+  const initializeGame = () => {
+    //set Current Score to 0, set Level to 1
+    setCurrentScore(0);
+  };
+
+  return (
+    <div>
+      <Header currentScore = {currentScore} bestScore = {bestScore}/>
+      <Body />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
---
+++ react-memory-card/src/components/Body.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Card from "./Card";
+
+const Body = () => {
+  return (
+    <>
+      Body
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+};
+
+export default Body;
