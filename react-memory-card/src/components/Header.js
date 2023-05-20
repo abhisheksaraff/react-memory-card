c0429b46ecb9822133c1f97e4244652ee4f71313
a6hi <saraffabhishek@outlook.com>
2023-05-20 00:44:52 -0400

    added styling to header and made it fixed

--- react-memory-card/src/components/Header.js
+++ react-memory-card/src/components/Header.js
@@ -8,12 +8,15 @@ const Header = ({
   totalLives,
 }) => {
   return (
-    <div className="header">
-      <div>Level = {currentLevel}</div>
-      <div>Score = {currentScore}</div>
-      <div>Best Score = {bestScore}</div>
-      {codeIsActivated && <div>Lives Left = {totalLives}</div>}
-    </div>
+    <nav class="header navbar fixed-top navbar-dark bg-dark">
+      <a class="navbar-brand">Memory Card Game</a>
+      <span class="navbar-text">
+        Level: {currentLevel} | Current Score: {currentScore} | Best Score: {bestScore}
+        {codeIsActivated && (
+          <span class="navbar-text"> | Lives Left: {totalLives}</span>
+        )}
+      </span>
+    </nav>
   );
 };
 
